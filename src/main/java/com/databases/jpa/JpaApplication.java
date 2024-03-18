5590f80c18eae2cc0d779431c534e9e340181a97
marlone-nyapwere <marloneenyapwere@gmail.com>
2024-03-18 16:23:47 +0200

    Sorting

--- src/main/java/com/databases/jpa/JpaApplication.java
+++ src/main/java/com/databases/jpa/JpaApplication.java
@@ -1,9 +1,11 @@
 package com.databases.jpa;
 
+import com.github.javafaker.Faker;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.Sort;
 
 
 @SpringBootApplication
@@ -16,8 +18,30 @@ public class JpaApplication {
 	@Bean
 	CommandLineRunner commandLineRunner(StudentRepository studentRepository) {
 		return args -> {
+			generateRandomStudents(studentRepository);
+			Sort sort = Sort.by(Sort.Direction.DESC, "firstName");
+			Sort sort2 = Sort.by( "firstName")
+					.ascending().
+					and(Sort.by("age").descending());
 
+			studentRepository.findAll(sort2)
+					.forEach(student -> System.out.println(student.getFirstName() + " " + student.getAge()));
 		};
+	}
+
+	private void generateRandomStudents(StudentRepository studentRepository) {
+			Faker faker = new Faker();
+
+			for (int i = 0; i < 20; i++) {
+				String firstName = faker.name().firstName();
+				String lastName = faker.name().lastName();
+				String email = String.format("%s.%s@email.com", firstName, lastName);
+				Student student = new Student(firstName,
+						lastName,
+						email,
+						faker.number().numberBetween(17, 55));
+				studentRepository.save(student);
+			}
 	}
 
 }

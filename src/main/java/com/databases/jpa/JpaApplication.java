113a8b00b36a80cb7e34c75659e93950a1794fb2
marlone-nyapwere <marloneenyapwere@gmail.com>
2024-03-18 15:02:56 +0200

    JPA methods

--- src/main/java/com/databases/jpa/JpaApplication.java
+++ src/main/java/com/databases/jpa/JpaApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import java.util.List;
+
 @SpringBootApplication
 public class JpaApplication {
 
@@ -15,11 +17,47 @@ public class JpaApplication {
 	@Bean
 	CommandLineRunner commandLineRunner(StudentRepository studentRepository) {
 		return args -> {
-			Student marlon = new Student("Marlon",
+			Student marlon = new Student(
+					"Marlon",
 					"N",
 					"marlone@email.com",
 					21);
+
+			Student sharon = new Student(
+					"Sharon",
+					"N",
+					"sharon@email.com",
+					25);
 			studentRepository.save(marlon);
+
+			System.out.println("Adding marlon and sharon");
+			studentRepository.saveAll(List.of(marlon, sharon));
+
+			System.out.println("Number of Students");
+			System.out.println(studentRepository.count());
+
+			System.out.println("Find Student 2 By ID");
+			studentRepository
+					.findById(2L)
+					.ifPresentOrElse(System.out::println, () -> {
+				System.out.println("Student with ID 2 not found");
+			});
+
+			System.out.println("Find Student 3 By ID");
+			studentRepository
+					.findById(3L)
+					.ifPresentOrElse(System.out::println, () -> {
+						System.out.println("Student with ID 2 not found");
+					});
+
+			System.out.println("Find all Students");
+			List<Student> students = studentRepository.findAll();
+			students.forEach(System.out::println);
+
+
+			System.out.println("Delete Student by ID");
+			studentRepository.deleteById(1L);
+			System.out.println(studentRepository.count());
 		};
 	}
 

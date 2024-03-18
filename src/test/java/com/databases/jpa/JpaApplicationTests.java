8a4bda0095719404f94b3e96caa9148b38e64087
marlone-nyapwere <marloneenyapwere@gmail.com>
2024-03-18 12:33:43 +0200

    initial setup

---
+++ src/test/java/com/databases/jpa/JpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.databases.jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

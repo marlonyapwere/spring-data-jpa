9335737941c2f32f6e5dcb932926e9444953e604
marlone-nyapwere <marloneenyapwere@gmail.com>
2024-03-18 16:01:16 +0200

    Installing Faker

--- src/main/java/com/databases/jpa/JpaApplication.java
+++ src/main/java/com/databases/jpa/JpaApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-import java.util.List;
 
 @SpringBootApplication
 public class JpaApplication {
@@ -17,39 +16,7 @@ public class JpaApplication {
 	@Bean
 	CommandLineRunner commandLineRunner(StudentRepository studentRepository) {
 		return args -> {
-			Student marlon = new Student(
-					"Marlon",
-					"N",
-					"marlone@email.com",
-					21);
 
-			Student sharon = new Student(
-					"Sharon",
-					"N",
-					"sharon@email.com",
-					25);
-
-			Student sharon2 = new Student(
-					"Sharon2",
-					"N",
-					"sharo@email.com",
-					25);
-
-			studentRepository.saveAll(List.of(marlon, sharon, sharon2));
-
-			studentRepository
-					.findStudentByEmail("sharon@email.com")
-					.ifPresentOrElse(System.out::println, () -> {
-						System.out.println("Student with specified email not found");
-					});
-
-			studentRepository.findStudentsByFirstNameEqualsAndAgeIsGreaterThanEqualNative(
-					"sharon",
-					25
-			).forEach(System.out::println);
-
-			System.out.println("deleting 3L");
-			System.out.println(studentRepository.deleteStudentById(3L));
 		};
 	}
 
